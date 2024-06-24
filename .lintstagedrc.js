67560a2181c915b854e88f39b598f4801b4b04a1
SP <spcfxda@gmail.com>
2024-06-24 08:58:08 +0000

    small update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,20 +2,20 @@ const path = require("path");
 
 const buildNextEslintCommand = (filenames) =>
   `yarn next:lint --fix --file ${filenames
-    .map((f) => path.relative(path.join("packages", "nextjs"), f))
+    .map((f) => path.relative(path.join("packages", "frontend"), f))
     .join(" --file ")}`;
 
 const checkTypesNextCommand = () => "yarn next:check-types";
 
 const buildHardhatEslintCommand = (filenames) =>
   `yarn hardhat:lint-staged --fix ${filenames
-    .map((f) => path.relative(path.join("packages", "hardhat"), f))
+    .map((f) => path.relative(path.join("packages", "backend"), f))
     .join(" ")}`;
 
 module.exports = {
-  "packages/nextjs/**/*.{ts,tsx}": [
+  "packages/frontend/**/*.{ts,tsx}": [
     buildNextEslintCommand,
     checkTypesNextCommand,
   ],
-  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
+  "packages/backend/**/*.{ts,tsx}": [buildHardhatEslintCommand],
 };
